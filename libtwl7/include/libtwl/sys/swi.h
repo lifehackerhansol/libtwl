d1a830f42b328a81c408ab482ef49796bea2afe8
Gericom <fnouwt2@gmail.com>
2024-05-17 22:25:59 +0200

    Added swi_getCrc16

--- libtwl7/include/libtwl/sys/swi.h
+++ libtwl7/include/libtwl/sys/swi.h
@@ -5,6 +5,7 @@ extern "C" {
 #endif
 
 extern void swi_waitByLoop(u32 iterations);
+extern u16 swi_getCrc16(u16 initialCrcValue, const void* src, u32 length);
 
 #ifdef __cplusplus
 }
--- libtwl7/source/sys/swi.s
+++ libtwl7/source/sys/swi.s
@@ -6,3 +6,9 @@
 swi_waitByLoop:
     swi #3
     bx lr
+
+.global swi_getCrc16
+.type swi_getCrc16, %function
+swi_getCrc16:
+    swi #0xE
+    bx lr
